de86b274f105e192fa5ba1a1c60aebd48b1b271b
Dan Constantinescu <dan.constantinescu@softvision.ro>
2017-10-09 10:26:03 +0300

    included the router, route and history

---
+++ src/components/Resources.js
@@ -0,0 +1,15 @@
+import React,{Component} from 'react';
+
+export default class Resources extends Component {
+    render(){
+        return(
+            <div>
+                <ul>
+                    <li>dasdas</li>
+                    <li>saf</li>
+                    <li>ghhhhh</li>
+                </ul>
+            </div>
+        );
+    }
+}
--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {BrowserRouter} from 'react-router-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
+import createBrowserHistory from 'history/createBrowserHistory'
+import {Router} from 'react-router';
+import Resources from './components/Resources';
+
+const history = createBrowserHistory();
 
 // import reducers from './reducers';
 // import {Provider} from 'react-redux';
@@ -13,13 +18,18 @@ import {BrowserRouter} from 'react-router-dom';
 // ReactDOM.render(
 //     <Provider store={createStoreWithMiddleware(reducers)}>
 //         <App />
-//     </Provider>
+//     </Provider >
 //     , document.getElementById('root'));
 
 
 ReactDOM.render(
     <BrowserRouter>
-        <App />
+        <Router history={history}>
+            <div>
+                <Route path='/' component={App}></Route>
+                <Route path='/resources' component={Resources}></Route>
+            </div>
+        </Router>
     </BrowserRouter>
     
     , document.getElementById('root'));
